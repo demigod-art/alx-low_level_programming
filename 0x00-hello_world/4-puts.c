a040e1fe89b86a4fc76d248e5020584722ad9b0d
demigod-art <mdauda1465@gmail.com>
2022-06-30 14:21:06 -0700

    c programm

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+/*
+ * File 4-put.c
+ * Auth: Brennan D Baraban <375@Holbertonschool.com>
+ */
+#include <stdio.h>
+/* main -programming is like a puzzle
+ */
+int main(void)
+{
+ puts("programming is loke building multi millionaire puzzle \n");
+ return(0);
+		 
+}
