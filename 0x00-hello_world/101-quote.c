902bf63994e53165b804cb3293a6cb0ed05675b6
demigod-art <mdauda1465@gmail.com>
2022-06-30 14:59:36 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,7 @@
  *  * File: 101-quote.c
  *   * Auth: Brennan D Baraban
  *    */
-
 #include <unistd.h>
-
 /**
  *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *   *                followed by a new line, to standard error.
@@ -16,6 +14,5 @@ int main(void)
 		write(2,
 					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 					      	      59);
-
 			return (1);
 }
