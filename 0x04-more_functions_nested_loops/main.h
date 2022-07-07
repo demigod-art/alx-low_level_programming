884faa3251f2959ff9675d98f5e95ac93cfc082e
demigod-art <mdauda1465@gmail.com>
2022-07-07 14:16:58 -0700

    y

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- * used in the 0x03-more_functions_nested_loops directory.
- */
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
