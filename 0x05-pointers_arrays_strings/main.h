615ba61fbbbf5fb20a1acdfdd7d52de236235610
demigod-art <mdauda1465@gmail.com>
2022-07-12 11:13:13 -0700

    🤔🤔

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,24 +1,26 @@
-#ifndef MAIN_H
+#ifndef MAIN-H
 #define MAIN_H
 
-/*
- * File: main.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- * used in the 0x04-more_functions_nested_loops directory.
- */
+/**
+* File: main.h
+* Auth: Dauda Renkre Mohammed
+* Desc: Header file containing prototypes for all functions
+* used in the 0x04-pointers_arrays_strings directory.
+*/
+
+#include <stdlib.h>
 
 int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+size_t _strlen(const char *str);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
