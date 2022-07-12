747eed33e5e271fa3ba2acf6e8ec9f05b0fd386a
demigod-art <mdauda1465@gmail.com>
2022-07-12 11:41:06 -0700

    fourth file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,4 @@
-#include "main.h"
+include "main.h"
 
 /**
  * print_rev - Prints a string in reverse.
@@ -6,13 +6,13 @@
  */
 void print_rev(char *s)
 {
-int len = 0, index;
+	int len = 0, index;
 
-while (s[index++])
-len++;
+	while (s[index++])
+		len++;
 
-for (index = len - 1; index >= 0; index--)
-_putchar(s[index]);
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
 
-_putchar('\n');
+	_putchar('\n');
 }
