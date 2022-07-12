ba79b8dd520f0d1a63108562bd8c22c53c170318
demigod-art <mdauda1465@gmail.com>
2022-07-12 11:16:03 -0700

    header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,26 +1,24 @@
-#ifndef MAIN-H
+#ifndef MAIN_H
 #define MAIN_H
 
-/**
-* File: main.h
-* Auth: Dauda Renkre Mohammed
-* Desc: Header file containing prototypes for all functions
-* used in the 0x04-pointers_arrays_strings directory.
-*/
-
-#include <stdlib.h>
+/*
+ * File: main.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x04-more_functions_nested_loops directory.
+ */
 
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
