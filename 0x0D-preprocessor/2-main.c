ede1ff134989d16c5c1247dfe1f22911158a0345
demigod-art <mdauda1465@gmail.com>
2022-08-01 04:41:47 -0700

    defining working file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * this is a simply file
+ * it just prrint the present working directory
+ */
+
+int main(void)
+{
+	printf("this is the present file: %d\n", __file__);
+
+	return (0);
+}
